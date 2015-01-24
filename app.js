d481b524770d43e2546c1cb6210d45d9cb490dc4
Beppu Kaoru <beppu@f.ait.kyushu-u.ac.jp>
2015-01-25 02:26:01 +0900

    新しいTODOの作成と表示

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-angular.module("App", []);
+angular.module("App", [])
+.controller("MainController", ["$scope", function($scope) {
+	// TODOリスト
+	$scope.todos = [];
+
+	// TODOリストへの追加
+	$scope.addTodo = function () {
+		$scope.todos.push({
+			title: Math.random(),
+			done: false
+		});
+	};
+}]);
