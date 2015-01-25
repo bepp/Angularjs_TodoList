82d47d9419dfb14b3691f203c87977f306b113bd
Beppu Kaoru <beppu@f.ait.kyushu-u.ac.jp>
2015-01-25 11:31:39 +0900

    リストのフィルタリング及びボタンの有効・無効

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ angular.module("App", [])
 	// TODOのタイトル
 	$scope.newTitle = "";
 
+	// フィルタリング条件モデル
+	$scope.filter = {
+		done: {done: true},	// 完了
+		remaining: {done: false}	// 未完了
+	}
+
+	// 現在のフィルタリング条件
+	$scope.currentFilter = null;
+
 	// TODOリストへの追加
 	$scope.addTodo = function () {
 		$scope.todos.push({
@@ -14,4 +23,19 @@ angular.module("App", [])
 		});
 		$scope.newTitle = "";
 	};
+
+	// フィルタリング条件変更
+	$scope.changeFilter = function(filter) {
+		$scope.currentFilter = filter;
+	};
+
+	// フィルタリング条件によりボタンを無効化
+	$scope.disabledButton = function(filter) {
+		if($scope.currentFilter == filter) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }]);
